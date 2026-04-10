41cce19ca21ebf076061b213e81862b5cdd3d3cc
Lzdevmendes <lzmendestechdev@gmail.com>
2026-04-09 21:22:48 -0300

    feat: add create order server action

---
+++ src/app/[slug]/menu/actions.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { ConsumptionMethod } from "@prisma/client";
+
+import { CartItem } from "@/contexts/cart";
+import { db } from "@/lib/prisma";
+
+interface CreateOrderInput {
+  slug: string;
+  restaurantId: string;
+  consumptionMethod: ConsumptionMethod;
+  items: CartItem[];
+}
+
+export const createOrder = async ({
+  restaurantId,
+  consumptionMethod,
+  items,
+}: CreateOrderInput): Promise<{ orderId: number; slug: string }> => {
+  const total = items.reduce(
+    (acc, item) => acc + item.product.price * item.quantity,
+    0,
+  );
+
+  const order = await db.order.create({
+    data: {
+      total,
+      status: "PENDING",
+      consumptionMethod,
+      restaurantId,
+      orderProducts: {
+        createMany: {
+          data: items.map((item) => ({
+            productId: item.product.id,
+            quantity: item.quantity,
+            price: item.product.price,
+          })),
+        },
+      },
+    },
+    include: { restaurant: true },
+  });
+
+  return { orderId: order.id, slug: order.restaurant.slug };
+};
